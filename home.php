ee7a29c91817ada7d1d0551e6fc6b76a6b986c6f
gavinvipe <110904713+gavinvipe@users.noreply.github.com>
2025-08-11 04:27:40 +0000

    Update home.php

--- home.php
+++ home.php
@@ -14,9 +14,9 @@ if (!isset($_SESSION['student_id'])) {
     <meta charset="UTF-8">
     <title>Student Portal | School Management</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bootstrap-5.3.7-dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <script src="bootstrap-5.3.7-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
 </head>
 <body class="bg-body text-body">
@@ -190,4 +190,5 @@ if ($regRes) {
 </div>
 
 </body>
+
 </html>
